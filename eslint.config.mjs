04608cd021b62fd19b6d50834ec9ecef9bd90452
Dhruv Dawar <160855360+dhruv-developer@users.noreply.github.com>
2025-03-10 19:23:54 +0530

    😭 I willll diee

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,14 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-empty-object-type": "off",
+      "react/react-in-jsx-scope": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
